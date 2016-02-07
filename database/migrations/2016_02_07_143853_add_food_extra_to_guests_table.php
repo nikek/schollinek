c8f799b118f792ca89e914fc40c85389ae36dc7b
Niklas Ek <nikek@spotify.com>
2016-02-07 15:44:19 +0100

    [migration] add food_extra to guests

---
+++ database/migrations/2016_02_07_143853_add_food_extra_to_guests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFoodExtraToGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->text('food_extra')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->dropColumn('food_extra');
+        });
+    }
+}
